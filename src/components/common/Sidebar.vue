b776b2eae93fa6f9f0cf78cbc4ef0bf02a768fa8
xdh <2624473038@qq.com>
2017-11-03 14:06:50 +0800

    第一次我

---
+++ src/components/common/Sidebar.vue
@@ -0,0 +1,150 @@
+<template>
+    <div class="sidebar">
+        <el-menu :default-active="onRoutes" class="el-menu-vertical-demo" theme="dark" unique-opened router>
+            <template >
+                <el-menu-item :index="items[0].index">
+                    <i :class="items[0].icon"></i>{{ items[0].title }}
+                </el-menu-item>
+            </template>
+            <template v-if="!!username" v-for="item in items2">
+                <template v-if="item.subs">
+                    <el-submenu :index="item.index">
+                        <template slot="title"><i :class="item.icon"></i>{{ item.title }}</template>
+                        <el-menu-item v-for="(subItem,i) in item.subs" :key="i" :index="subItem.index">{{ subItem.title
+                            }}
+                        </el-menu-item>
+                    </el-submenu>
+                </template>
+                <template v-else>
+                    <el-menu-item :index="item.index">
+                        <i :class="item.icon"></i>{{ item.title }}
+                    </el-menu-item>
+                </template>
+            </template>
+
+        </el-menu>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+
+            return {
+                ok: true,
+
+                items: [
+                    {
+                        icon: 'el-icon-setting',
+                        index: 'readme',
+                        title: '自述'
+                    },
+                    {
+                        icon: 'el-icon-setting ',
+                        index: '',
+                        title: '提交作业'
+                    },
+                    {
+                        icon: 'el-icon-menu',
+                        index: '2',
+                        title: '表格',
+                        subs: [
+                            {
+                                index: 'basetable',
+                                title: '基础表格'
+                            },
+                            {
+                                index: 'vuetable',
+                                title: 'Vue表格组件'
+                            }, {
+                                index: 'basetable',
+                                title: '基础表格'
+                            }
+                        ]
+                    },
+                    {
+                        icon: 'el-icon-date',
+                        index: '3',
+                        title: '表单',
+                        subs: [
+                            {
+                                index: 'baseform',
+                                title: '基本表单'
+                            },
+                            {
+                                index: 'vueeditor',
+                                title: '编辑器'
+                            },
+                            {
+                                index: 'markdown',
+                                title: 'markdown'
+                            },
+                            {
+                                index: 'upload',
+                                title: '文件上传'
+                            }
+                        ]
+                    },
+
+                    {
+                        icon: 'el-icon-star-on',
+                        index: 'basecharts',
+                        title: '图表'
+                    },
+                    {
+                        icon: 'el-icon-upload2',
+                        index: 'drag',
+                        title: '拖拽'
+                    }, {
+                        icon: 'el-icon-document',
+                        index: '4',
+                        title: '学生信息查询',
+                        subs: [
+
+                            {
+                                index: 'studentfiles',
+                                title: '学员档案'
+                            },
+                            {
+                                index: 'basecharts',
+                                title: '学生信息2'
+                            }, {
+                                index: 'upload',
+                                title: '学生信息3'
+
+                            }]
+
+                    }
+                ]
+            }
+        },
+        computed: {
+            onRoutes(){
+                return this.$route.path.replace('/', '');
+            },
+            username(){
+                let username = localStorage.getItem('ms_username');
+                return username;
+            },
+            items2(){
+                return this.items.splice(1);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .sidebar {
+        display: block;
+        position: absolute;
+        width: 250px;
+        left: 0;
+        top: 70px;
+        bottom: 0;
+        background: #2E363F;
+    }
+
+    .sidebar > ul {
+        height: 100%;
+    }
+</style>
